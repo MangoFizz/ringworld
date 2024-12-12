2e0cf7dd76e82833faad270d102242771d2cf781
Sledmine <40512223+Sledmine@users.noreply.github.com>
2024-12-11 23:57:46 -0600

    add unit delete all weapons binding

---
+++ include/ringworld/units/units.h
@@ -0,0 +1,8 @@
+#ifndef RINGWORLD__UNITS__UNITS_H
+#define RINGWORLD__UNITS__UNITS_H
+
+#include <stdint.h>
+
+void unit_delete_all_weapons(uint32_t unit_handle);
+
+#endif // RINGWORLD__UNITS__UNITS_H

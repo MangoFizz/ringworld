076dda2040d48b5e506c6f3eed0e32c14aa57b8a
MangoFizz <ccs81.mr@gmail.com>
2024-12-15 10:01:56 +0000

    It should calculate the thing before the table is initialized

--- src/impl/memory/table.c
+++ src/impl/memory/table.c
@@ -26,7 +26,7 @@ extern uint32_t *game_state_checksum;
 void *create_table(const char *name, uint16_t maximum_count, uint16_t element_size) {
     size_t allocation_amount = CALCULATE_ALLOCATION_SIZE(maximum_count, element_size);
     GenericTable *table = (GenericTable *)(allocate_heap(allocation_amount));
-    init_table(table, name, maximum_count, element_size);
     malloc_crc_checksum_buffer(game_state_checksum, &maximum_count, 4);
+    init_table(table, name, maximum_count, element_size);
     return table;
 }

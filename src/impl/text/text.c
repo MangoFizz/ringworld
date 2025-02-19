92225df7b7d8b2f80d21ab661d170d461e666aac
MangoFizz <ccs81.mr@gmail.com>
2025-02-19 13:49:58 +0000

    text drawing globals

---
+++ src/impl/text/text.c
@@ -0,0 +1,7 @@
+#include "text.h"
+
+extern TextDrawGlobals *text_drawing_globals;
+
+TextDrawGlobals *text_get_drawing_globals(void) {
+    return text_drawing_globals;
+}
--- src/impl/text/text.h
+++ src/impl/text/text.h
@@ -5,10 +5,22 @@
 extern "C" {
 #endif
 
+#include "../tag/tag.h"
 #include "../types/types.h"
 
+typedef struct TextDrawGlobals {
+    uint32_t string_list_index;
+    TagHandle font;
+    uint32_t unknown1;
+    int16_t unknown_ffff;
+    int16_t justification;
+    ColorARGB color;
+    int16_t tabs[0x12];
+    VectorXYInt offset;
+} TextDrawGlobals;
+
 /**
- * This function draws a Unicode string on the screen.
+ * This function draws a unicode string on the screen.
  * @param draw_character A function pointer to the character drawing function.
  * @param position A pointer to the Rectangle2D structure defining the screen offset.
  * @param color The color of the text in ARGB format.
@@ -16,7 +28,24 @@ extern "C" {
  * @param flags Flags to control the text drawing behavior.
  * @param string The Unicode string to be drawn.
  */
-bool text_draw_unicode_string(void (*draw_character)(), Rectangle2D *position, ColorARGBInt color, Rectangle2D *draw_rect, uint32_t flags, wchar_t *string);
+bool text_draw_unicode_string(void (*draw_character)(), Rectangle2D *position, ColorARGBInt *color, Rectangle2D *draw_rect, uint32_t flags, wchar_t *string);
+
+/**
+ * This function draws a string on the screen.
+ * @param draw_character A function pointer to the character drawing function.
+ * @param position A pointer to the Rectangle2D structure defining the screen offset.
+ * @param color The color of the text in ARGB format.
+ * @param draw_rect A pointer to the Rectangle2D structure defining the screen bounds.
+ * @param flags Flags to control the text drawing behavior.
+ * @param string The string to be drawn.
+ */
+bool text_draw_string(void (*draw_character)(), Rectangle2D *position, ColorARGBInt *color, Rectangle2D *draw_rect, uint32_t flags, char *string);
+
+/**
+ * This function retrieves the global text drawing settings.
+ * @return A pointer to the TextDrawGlobals structure containing the current text drawing settings.
+ */
+TextDrawGlobals *text_get_drawing_globals(void);
 
 #ifdef __cplusplus
 }

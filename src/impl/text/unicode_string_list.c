895910c674c8763b759081efb0decb4c2cebedd2
MangoFizz <ccs81.mr@gmail.com>
2025-02-26 01:25:40 +0000

    Check if the null terminator is where it should be

--- src/impl/text/unicode_string_list.c
+++ src/impl/text/unicode_string_list.c
@@ -11,7 +11,10 @@ const wchar_t *unicode_string_list_get_string_or_null(TagHandle unicode_string_l
                 const char *tag_path = tag_get_path(unicode_string_list_tag);
                 CRASHF_DEBUG("Unicode string list string at index %d in tag %s is NULL", string_index, tag_path);
             }
-            return string;
+            size_t string_size = string_list_item->string.size / sizeof(wchar_t);
+            if(string[string_size - 1] == L'\0') {
+                return string;
+            }
         }
     }
     return NULL;
